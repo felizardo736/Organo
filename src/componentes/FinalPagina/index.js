ce5003e28bf5b16394f261511cb76ee64d0da99a
Luiz Felipe Felizardo <126994107+felizardo736@users.noreply.github.com>
2023-11-08 20:22:44 -0300

    public

---
+++ src/componentes/FinalPagina/index.js
@@ -0,0 +1,28 @@
+import './FinalPagina.css'
+
+const FinalPagina = () => {
+    return(
+        <header className="fundo">
+            <div className='logos'>
+            <div className='fb'>
+                <img src='/imagens/fb.png' alt='logo facebook'/>
+            </div>
+            <div className='tw'>
+                <img src='/imagens/tw.png' alt='logo twitter'/>
+            </div>
+            <div className='ig'>
+                <img src='/imagens/ig.png' alt='logo instagram'/>
+            </div>
+            <div className='logo'>
+                <img src='/imagens/logo.png' alt='logo organo'/>
+            </div>
+            </div>
+            <div>
+                <img src='/imagens/fundo.png' alt='Final da página' />
+            </div>
+            
+        </header>
+    )
+}
+
+export default FinalPagina
